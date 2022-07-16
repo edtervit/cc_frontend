324ac3d8f1277fce37f2cfa346a94e2c4fda7c49
Ed Tervit <edtervit@hotmail.co.uk>
2022-07-16 16:00:53 +0100

    info popover

--- components/ObliqueCard.tsx
+++ components/ObliqueCard.tsx
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import ShuffleButton from "./ShuffleButton";
+import {Popover} from 'react-tiny-popover'
 
-function ObliqueCard({ generalMessages }: { generalMessages: any }) {
+
+function ObliqueCard({generalMessages}: {generalMessages: any}) {
   const [messageCounter, setMessageCounter] = useState(0);
   const [message, setMessage] = useState(
     generalMessages[messageCounter].message
   );
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false)
 
   const newMessageHandler = () => {
     if (messageCounter + 1 == generalMessages.length) {
@@ -29,7 +32,23 @@ function ObliqueCard({ generalMessages }: { generalMessages: any }) {
 
   return (
     <>
-      <h2 className="text-lg text-center mb-4">Oblique Strategies</h2>
+      <div className="flex justify-center items-center mb-4">
+        <h2 className="text-lg text-center">Oblique Strategies</h2>
+        <Popover
+          isOpen={isPopoverOpen}
+          padding={5}
+          positions={['right', 'top']}
+          onClickOutside={() => setIsPopoverOpen(false)}
+          content={
+            <div className="bg-white rounded-lg px-3 py-1 text-sm transition-all">
+              <a className="cursor-pointer underline" href="https://en.wikipedia.org/wiki/Oblique_Strategies" target='_blank'>Click here to learn more about oblique strategies.</a>
+            </div>
+          }
+        >
+          <img className="w-4 cursor-pointer ml-1" src="./icons/info.svg" onClick={() => setIsPopoverOpen(!isPopoverOpen)} />
+        </Popover>
+      </div>
+
       <div className="flex flex-col flex-1 w-full items-center justify-center text-center bg-white text-black rounded-3xl min-h-max p-5 shadow-gray-400 shadow-lg ">
         <p className="bebe text-2xl mt-auto">{message}</p>
         <div className="mt-auto" onClick={() => newMessageHandler()}>
